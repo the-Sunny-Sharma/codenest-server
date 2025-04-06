8caf64e682aee4bb9589808b402dbeec8c96a1f5
Sunny Sharma <syn8433898723@gmail.com>
2025-04-06 18:43:54 +0530

    Debug logger function added

--- app.js
+++ app.js
@@ -702,6 +702,15 @@ const io = new Server(server, {
   },
 });
 
+// Create a debug logger function that respects environment
+const debug = (namespace, message, data = null) => {
+  if (!isProduction || process.env.DEBUG === "true") {
+    const timestamp = new Date().toISOString();
+    console.log(`[${timestamp}] [${namespace}] ${message}`);
+    if (data) console.log(data);
+  }
+};
+
 // Import models
 import HelpRequest from "./models/HelpRequest.js";
 import CodeSession from "./models/CodeSession.js";
